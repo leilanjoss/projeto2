fa258788a16780d9d86934d349e052909820bca2
ana julia <anajuliany@gmail.com>
2023-07-03 19:54:08 -0300

    js pronto

--- barradefiltro/script.js
+++ barradefiltro/script.js
@@ -1,23 +1,25 @@
-const selectQtdeAnuncios = document.getElementById('qtde-anuncios');
-    selectQtdeAnuncios.addEventListener('change', filtrarCarros);
+// Acessa as opções de quantidade de anuncios 
+const selectQtdeAnuncios = document.getElementById('qtde-anuncios'); 
+// Adiciona um eventListener pra mudar quando a função filtrarCarros for chamada
+selectQtdeAnuncios.addEventListener('change', filtrarCarros); 
 
-    // Função para filtrar os carros com base na quantidade selecionada
-    function filtrarCarros() {
-        // Obtém o valor selecionado
-        const qtdeSelecionada = selectQtdeAnuncios.value;
+// Função para filtrar os carros com base na quantidade selecionada
+function filtrarCarros() {
+    // Obtém o valor selecionado
+    const qtdeSelecionada = selectQtdeAnuncios.value;
+    // Acessa todos os elementos com a classe "car"
+    const carros = document.querySelectorAll('.car');
 
-        // Obtém todos os elementos com a classe "car"
-        const carros = document.querySelectorAll('.car');
+    // Oculta todos os carros, acessando cada um deles e ocultando um por um
+    carros.forEach(carro => { 
+        carro.style.display = 'none';
+    });
 
-        // Oculta todos os carros
-        carros.forEach(carro => {
-            carro.style.display = 'none';
-        });
-
-        // Mostra apenas a quantidade selecionada de carros
-        for (let i = 0; i < qtdeSelecionada; i++) {
-            if (carros[i]) {
-                carros[i].style.display = 'flex';
-            }
-        }
+    // Mostra apenas a quantidade selecionada de carros, então sempre que o indice for menor que qtdeSelecionada, ele acrescenta mais um
+    for (let i = 0; i < qtdeSelecionada; i++) {
+        // Verifica se o carro existe no indice, e caso exista, mostra os carros em display "flex"
+        if (carros[i]) {
+            carros[i].style.display = 'flex';
+       }
     }
+}
